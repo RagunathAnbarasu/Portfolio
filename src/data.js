d831be5880f362805de43b9beb5b5d7065ff8d26
“ragu” <buzzragu@gamil.com>
2021-10-21 01:41:47 -0400

    Updating testimonial data

--- src/data.js
+++ src/data.js
@@ -37,14 +37,14 @@ export const testimonials = [
   {
     quote:
       "I have known Ragunath for quite a few years now. Exploring and being eager to learn new things, adds value to his active and open mindset. He is one of the few people I know who always is up-to-date with technological, business and social aspects. An ideal candidate for critical thinking.",
-    image: "",
+    image: "https://media-exp1.licdn.com/dms/image/C5103AQEXcxhdk2Wexg/profile-displayphoto-shrink_100_100/0/1562434086333?e=1640217600&v=beta&t=NJUUMpYPSGGki57c0_fhcL5ZvBGfIpD7d-tNm-lYD6A",
     name: "Pasupathi Subramanian",
     company: "Allstate India",
   },
   {
     quote:
       "Ragunath volunteered as a webmaster in IEEE WIE Ottawa during 2020 under my leadership. He is active, always has new ideas and initiatives. Not only he has helped in creating a new website for IEEE WIE Ottawa, but he also was the main lead of the first hackathon organized by IEEE WIE Ottawa.",
-    image: "",
+    image: "https://media-exp1.licdn.com/dms/image/C5603AQGTaRP1VGBQDw/profile-displayphoto-shrink_100_100/0/1516837213950?e=1640217600&v=beta&t=H7wmGvFDvAXJcKNDR5r0JXCsN8YlREd4kBaH_6mEpmk",
     name: "Anastassia Gharib",
     company: "2021 IEEE WIE Voting Committee Member",
   },
